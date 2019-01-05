4912e524505081abc6ead082b49266be422bff94
bilalshahzad139 <bilal.shahzad@yahoo.com>
2019-01-06 01:00:00 +0500

    Part_1_2

---
+++ Part_1_2/NUnitPrac2/Magician.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitPrac2
+{
+    public class Magician
+    {
+        public int DoMagic(String param)
+        {
+            if (param.Equals("ABC"))
+                return 10;
+            else if (param.Equals("XYZ"))
+                return 20;
+            else
+                return 30;
+        }
+    }
+}
---
+++ Part_1_2/NunitPrac2.Tests/MagicianTest.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using NUnitPrac2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NunitPrac2.Tests
+{
+    [TestFixture]
+    public class MagicianTest
+    {
+        [Test]
+        public void DoMagic_Testing()
+        {
+            //Arrange
+            int expectedResult = 10;
+            String data = "ABC";
+
+            //Act
+            Magician obj = new Magician();
+            var r = obj.DoMagic(data);
+
+            Assert.AreEqual(r, expectedResult);
+        }
+
+        [TestCase("ABC",10)]
+        [TestCase("XYZ", 20)]
+        [TestCase("X", 30)]
+        [TestCase("", 30)]
+        public void DoMagic_Testing(String data, int expectedResult)
+        {
+            //Arrange
+            //int expectedResult = 10;
+
+            //Act
+            Magician obj = new Magician();
+            var r = obj.DoMagic(data);
+
+            Assert.AreEqual(r, expectedResult);
+        }
+
+    }
+}
